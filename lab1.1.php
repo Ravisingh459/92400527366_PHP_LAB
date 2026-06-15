d86e63c853b043a8f520eea5b801e957bdec3672
Ravisingh459 <rks0862004@gmail.com>
2026-06-15 09:57:13 +0530

    Add files via upload

---
+++ lab1.1.php
@@ -0,0 +1,20 @@
+<?php
+// Constant
+define("COLLEGE_NAME", "ABC College");
+
+// Variables
+$studentName = "Rahul Patel";
+$rollNo = 101;
+$semester = "Semester 3";
+$percentage = 78.5;
+$result = "PASS";
+
+// Display Result
+echo "<h2>Previous Semester Result</h2>";
+echo "College Name: " . COLLEGE_NAME . "<br>";
+echo "Student Name: " . $studentName . "<br>";
+echo "Roll Number: " . $rollNo . "<br>";
+echo "Semester: " . $semester . "<br>";
+echo "Percentage: " . $percentage . "%<br>";
+echo "Result: " . $result . "<br>";
+?>
