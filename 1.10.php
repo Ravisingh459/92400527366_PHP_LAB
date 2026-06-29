d1cde36577fb38904ec0e51514f722559bed0fd7
Ravisingh459 <rks0862004@gmail.com>
2026-06-29 23:22:33 +0530

    Add files via upload

---
+++ 1.10.php
@@ -0,0 +1,9 @@
+<?php
+$array1 = array("Apple", "Banana", "Mango");
+$array2 = array("Orange", "Grapes", "Pineapple");
+
+$mergedArray = array_merge($array1, $array2);
+
+echo "Merged Array:<br>";
+print_r($mergedArray);
+?>
