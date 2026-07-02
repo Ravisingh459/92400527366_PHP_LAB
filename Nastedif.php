b4fffe81a023da2e1920c735cf6b0caeb6d331cf
Ravi Singh <rks0862004@gmail.com>
2026-07-02 23:30:33 +0530

    Add files via upload

---
+++ Nastedif.php
@@ -0,0 +1,20 @@
+<?php
+
+	$collage="marwadi university";
+	$course="BCA";
+	if($college="marwadi university")
+	{
+		if($course=="BCA")
+		{
+			echo"nirasha";
+		}
+		else
+		{
+			echo"they are not in BCA";
+		}
+	}
+	else
+	{
+		echo"they didn't selected marwadi";
+	}
+?>
