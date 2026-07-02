27de30d16d334b88eb72d58884e7e099f080d866
Ravi Singh <rks0862004@gmail.com>
2026-07-02 23:28:58 +0530

    Add files via upload

---
+++ elseif.php
@@ -0,0 +1,16 @@
+<?php
+
+	$fruit="mango";
+	if($fruit=="mango")
+	{
+		echo "it is summer fruit";
+	}
+	else if($fruit=="apple")
+	{
+		echo "it is all season fruit";
+	}
+	else
+	{
+		echo"it is not fruit";
+	}
+?>
